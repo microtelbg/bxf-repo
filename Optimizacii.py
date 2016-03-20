19c5a9fff6aecd34e4f9f298b556bdfc5f51e929
Agnesa Stoyanova <agnesastoyanova@gmail.com>
2016-03-20 10:49:48 -0500

    Popravka v optimizacii

--- Optimizacii.py
+++ Optimizacii.py
@@ -3,7 +3,6 @@ Created on Mar 16, 2016
 
 @author: AS017303
 '''
-from itertools import permutations
 
 def raztoqnie(dupka1, dupka2):
     x1 = dupka1['x']
@@ -16,10 +15,22 @@ def raztoqnie(dupka1, dupka2):
 def total_raztoqnie(dupki):  
     return sum([raztoqnie(dupka, dupki[index + 1]) for index, dupka in enumerate(dupki[:-1])])
 
-def optimizirai_otvori(otvori, start=None):    
+def optimizirai_otvori(otvori, start=None):  
     if start is None:
         start = otvori[0]
-    return min([perm for perm in permutations(otvori) if perm[0] == start], key=total_raztoqnie)
+        must_visit = otvori
+        path = [start]
+        must_visit.remove(start)
+        while must_visit:
+            nearest = min(must_visit, key=lambda x: raztoqnie(path[-1], x))
+            path.append(nearest)
+            must_visit.remove(nearest)
+        return path
+    
+    # Tova e drug vid optimizacia pak za travelling salesman no e bavna. Izpolzva se samo za 7-8 otvora
+#         if start is None:
+#             start = otvori[0]
+#         return min([perm for perm in permutations(otvori) if perm[0] == start], key=total_raztoqnie)
 
 def is_dupka_duplicate(dupka, dupki):
     for d in dupki:
